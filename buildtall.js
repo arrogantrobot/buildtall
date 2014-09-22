b50442ae848757345a3a351529a5e3bd794873df
arrogantrobot <arrogantrobot@gmail.com>
2014-09-21 23:39:49 -0500

    add gitignore, working random widths

--- buildtall.js
+++ buildtall.js
@@ -8,11 +8,11 @@ function Layer(z, width, x, y) {
   this.buildings = new Array();
 
   //constants
-  this.colorScheme = [ "#FF0000", "#0FF00", "#0000FF" ];
+  this.colorScheme = new Array( "#FF0000", "#00FF00", "#0000FF" );
   this.maxBuildings = 20;
   this.minBuildings = 10;
   this.minBuildingWidth = 50;
-
+  this.numPoints = 50;
 
   this.addBuilding = function(building) {
     this.buildings.push(building);
@@ -27,51 +27,55 @@ function Layer(z, width, x, y) {
   this.populate = function() {
     var index;
     var points = new Array();
-    for (index = 0; index < this.maxBuildings; ++index) {
+    for (index = 0; index < this.numPoints; ++index) {
       points.push(Math.floor((Math.random() * this.width) + this.x));
     }
-    points.sort();
+    points.sort(function(a, b){return a-b});
     index = 0;
-    while (index < points.length) {
+    while (index < points.length - 1) {
       var left = points[index];
       index++;
       while ((points[index] - left) < this.minBuildingWidth) {
         index++;
       }
+      if (index >= points.length) return;
       this.addBuilding(new Building(
             left, 
-            points[index], 
+            points[index] - left, 
             200, 
-            this.colorScheme[Math.random() * this.colorScheme.length]
+            this.colorScheme[Math.floor(Math.random() * this.colorScheme.length)]
       ));
+      index++;
     }
   }
 
 }
 
 function Building(x, width, height, color) {
-  this.x = x;
-  this.width = width;
+  this.x_loc = x;
+  this.width_loc = width;
   this.height = height;
   this.color = color;
 
   this.draw = function(context) {
     var style = context.fillStyle;
+    console.log(this.x_loc + " " + this.width_loc + " " + this.color);
     context.fillStyle = this.color;
-    context.rect(this.x, 10, this.width, this.height);
-    context.fill();
+    context.fillRect(this.x_loc, 10, this.width_loc, this.height);
     context.fillStyle = style;
   }
 }
 
-function Buildtall(canvas) {
+function Scene(canvas) {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   this.canvas = canvas;
   this.context = canvas.getContext('2d');
   this.width = canvas.width;
   this.layers = new Array(new Layer(0, this.width)); 
+
   this.layers[0].populate();
+
   this.draw = function draw() {
     for (layer of this.layers) {
       layer.draw(this.context);
@@ -80,6 +84,6 @@ function Buildtall(canvas) {
 }
 
 function start(canvas) {
-  var bt = new Buildtall(canvas);
+  var bt = new Scene(canvas);
   bt.draw();
 }
